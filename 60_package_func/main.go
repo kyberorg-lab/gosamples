6d41df2a09a2f7c3b80605216cccbdaf86bb6cd6
alexander.muravya <kyberorg@yadev.eu>
2020-07-08 18:46:59 +0300

    [WIP] Package func aka external resource

--- 60_package_func/main.go
+++ 60_package_func/main.go
@@ -3,5 +3,12 @@ package main
 import "gitlab.com/rvasily/msu-go-11/1/60_package_func_doc/world"
 
 func main() {
+	world.PrintStartRoom()
+
+	//var from package
+	println("starting room: ", world.StartingRoom)
+
+	//same by function
+	println("starting room: ", world.GetStartingLevel())
 
 }
