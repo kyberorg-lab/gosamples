a357fc3d8187dd0ceaeb009f3158ef50e14e5552
alexander.muravya <kyberorg@yadev.eu>
2020-07-09 16:35:41 +0300

    Errors

---
+++ 95_errors/main.go
@@ -0,0 +1,70 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+)
+
+//Create a named type for our new error type
+type errorString string
+
+//implement the error interface
+func (e errorString) Error() string {
+	return string(e)
+}
+
+//New creates interface values of type error
+func New(text string) error {
+	return errorString(text)
+}
+
+var ErrNamedType = New("EOF")
+var ErrStructType = errors.New("EOF")
+
+type MyError struct {
+	Msg  string
+	File string
+	Line int
+}
+
+func (e *MyError) Error() string {
+	return fmt.Sprint("%s:%d:%s", e.File, e.Line, e.Msg)
+}
+
+func BadFunc() error {
+	return &MyError{"Something happened", "server.go", 42}
+}
+
+type temporary interface {
+	Temporary() bool
+}
+
+//IsTemporary return true if err is temporary
+func IsTemporary(err error) bool {
+	te, ok := err.(temporary)
+	return ok && te.Temporary()
+}
+
+func main() {
+	if ErrNamedType == New("EOF") {
+		fmt.Println("Named Type Error")
+	}
+
+	if ErrStructType == errors.New("EOF") {
+		fmt.Println("Struct Type Error")
+	}
+
+	err := BadFunc()
+	switch err := err.(type) {
+	case nil:
+	//call succeeded, nothing to do
+	case *MyError:
+		fmt.Println("error occurred on line:", err.Line)
+	default:
+		// unknown error
+	}
+
+	if IsTemporary(err) {
+		fmt.Println("Error is temporary")
+	}
+}
